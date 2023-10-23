28433e917175290c8d7b6f3ffdc800b3f0875d73
IreneChicca <irene.chicca@outlook.com>
2023-10-23 16:36:15 +0200

    Add index table

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -1,7 +1,44 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="container border rounded my-5">
 
-    @dump($projects)
+ <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Titolo</th>
+                <th scope="col">Data</th>
+                <th scope="col">Linguaggio</th>
+                <th scope="col">Commit</th>
+                <th scope="col">Bonus</th>
+              </tr>
+            </thead>
 
+    @forelse ($projects as $project)
+        
+    
+       
+            <tbody>
+              <tr>
+                <th scope="row">{{$project->id}}</th>
+                <td>{{$project->title}}</td>
+                <td>{{$project->date}}</td>
+                <td>{{$project->main_lang}}</td>
+                <td>{{$project->commit}}</td>
+                <td>{{$project->bonus}}</td>
+              </tr>
+
+    @empty
+
+        <p>Non ci sono progetti al momento..</p>
+
+    @endforelse
+
+             </tbody>
+          </table>
+
+    
+   </div>
+</div>
 @endsection
